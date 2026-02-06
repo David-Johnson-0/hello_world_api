ec1b95f85708cc3fe676c47e6e4e4c8813f42d5a
davidjohnson <soccerdavejohnson@gmail.com>
2026-02-05 18:54:01 -0600

    Finished message

--- hello_world_app/views.py
+++ hello_world_app/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello, World!</h1>" \
-    "<p>Welcome to the Django Hello World Application" \
-    "deployed from AWS EC2 instance to PostgreSQL database!" \
-    "The Author of this application is David Johnson as part" \
-    "of the class CSCI 4830.</p>")
+    return HttpResponse("<h1>Hello World, from David Johnson!</h1>" \
+    "<p>Welcome to the Django Hello World Application " \
+    "deployed from AWS EC2 instance to PostgreSQL database! " \
+    "The Author of this application is David Johnson as part " \
+    "of the class: Intro to Software Engineering - CSCI 4830.</p>")
