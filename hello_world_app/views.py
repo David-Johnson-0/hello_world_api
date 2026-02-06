e7c570f734ce031128e41e17be26a48481c26dec
davidjohnson <soccerdavejohnson@gmail.com>
2026-02-05 18:49:19 -0600

    added message page

--- hello_world_app/views.py
+++ hello_world_app/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello, World!</h1>")
+    return HttpResponse("<h1>Hello, World!</h1>" \
+    "<p>Welcome to the Django Hello World Application" \
+    "deployed from AWS EC2 instance to PostgreSQL database!" \
+    "The Author of this application is David Johnson as part" \
+    "of the class CSCI 4830.</p>")
